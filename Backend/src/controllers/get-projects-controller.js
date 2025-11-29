1799494b279ccb059f3ceca07ece55b8c16212df
Rosana-Moya <rosanamoya88@gmail.com>
2025-11-29 08:38:37 +0100

    getProjectsController

--- Backend/src/controllers/get-projects-controller.js
+++ Backend/src/controllers/get-projects-controller.js
@@ -1,11 +1,17 @@
 const mysql = require("../database/mysql-pool");
 
 const getProjects = async (req, res) => {
-    try {
+  try {
+    const query = "SELECT * FROM projects WHERE deleted_at IS NULL";
+    const connection = await mysql.getConnection();
+    const [data] = await connection.query(query);
+   
 
-    } catch {
-        res.send("Algo ha ido mal");
-    }
+    res.json(data);
+  } catch (err) {
+    console.error("Error al obtener los proyectos:", err);
+    res.status(400).json({ error: "No se han podido obtener los proyectos" });
+  }
 };
 
 

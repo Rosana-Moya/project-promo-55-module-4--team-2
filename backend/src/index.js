f992085ea64e5ec67b063b90ae0b7e84ad3f882c
Rosana-Moya <rosanamoya88@gmail.com>
2025-12-03 21:01:01 +0100

    finalizando

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { postProject, getProjects, getProjectId, patchProjectId } = require("./controllers");
 const app = express();
 const port = 3000;
@@ -17,3 +18,8 @@ app.post("/project", postProject);
 app.get("/projects", getProjects);
 app.get("/project/:id",getProjectId);
 app.patch("/delete-project/:id", patchProjectId);
+app.use("/project-promo-55-module-3-team-2", express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
